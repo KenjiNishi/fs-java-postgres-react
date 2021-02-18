170a5e1ca2880c0a6d0a414173cf8919a6e45ea2
KenjiNishi <wk.nishizawa@gmail.com>
2021-02-18 01:22:41 -0300

    Springboot framework

---
+++ src/main/java/com/nsw/postgresreact/PostgresreactApplication.java
@@ -0,0 +1,13 @@
+package com.nsw.postgresreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nsw/postgresreact/PostgresreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsw.postgresreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
