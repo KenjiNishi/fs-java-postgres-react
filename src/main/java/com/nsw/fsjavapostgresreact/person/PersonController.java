dbd6b645686df52b9b69e0285cd7520a9fb8040b
KenjiNishi <wk.nishizawa@gmail.com>
2021-02-19 23:16:11 -0300

    basic setup for one table, buildable

---
+++ src/main/java/com/nsw/fsjavapostgresreact/person/PersonController.java
@@ -0,0 +1,27 @@
+package com.nsw.fsjavapostgresreact.person.controllers;
+
+import com.nsw.fsjavapostgresreact.person.services.PersonService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping(path = "api/person")
+public class PersonController {
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+
+    @GetMapping
+	public String getPersons(){
+		return personService.getPersons();
+	}
+
+    
+}
---
+++ src/main/java/com/nsw/fsjavapostgresreact/person/PersonService.java
@@ -0,0 +1,12 @@
+package com.nsw.fsjavapostgresreact.person.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    
+	public String getPersons(){
+		return "Hellooo";
+	}
+}
