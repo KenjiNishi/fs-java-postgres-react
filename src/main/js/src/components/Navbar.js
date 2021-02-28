ad9acadcc755e133c9646bb8e8fb64a6380989a6
KenjiNishi <wk.nishizawa@gmail.com>
2021-02-28 01:44:11 -0300

    responsive navbar

--- src/main/js/src/components/Navbar.js
+++ src/main/js/src/components/Navbar.js
@@ -6,7 +6,11 @@ export default class Navbar extends Component {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">SpringBoot+React</Link>
-        <div className="collapse navbar-collapse">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
             <li className="navbar-item">
                 <Link to="/" className="nav-link">Dashboard</Link>
